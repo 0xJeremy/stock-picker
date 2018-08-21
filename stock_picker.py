3307a97d5b7588f9cbed174561e624894c5e1881
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-08-21 18:02:08 -0400

    Fixed email bug

--- stock_picker.py
+++ stock_picker.py
@@ -3,59 +3,64 @@ import smtplib
 import sys
 
 def email_data(data):
-    #server = smtplib.SMTP("smtp.gmail.com", 587)
-    #server.starttls()
-    #server.login([sending email], [password])
-    print("\nEmailing information...")
-    #server.sendmail([sending email], [target email], data)
-    #server.quit()
-    
+	#server = smtplib.SMTP("smtp.gmail.com", 587)
+	#server.starttls()
+	#server.login([sending email], [password])
+	print("\nEmailing information...")
+	#server.sendmail([sending email], [target email], data)
+	#server.quit()
+	print(data)
+	
 def get_news(stock):
-    newsURL = "https://api.iextrading.com/1.0/stock/" + stock + "/news/last/5"
-    news = requests.get(url = newsURL)
-    return news
+	newsURL = "https://api.iextrading.com/1.0/stock/" + stock + "/news/last/5"
+	news = requests.get(url = newsURL)
+	return news
 
 def check_move(stock, pos):
-    URL = "https://api.iextrading.com/1.0/stock/" + stock + "/quote/changePercent"
-    r = requests.get(url = URL)
-    
-    data = r.json()
-    data = data*100
-    if(data <= -5 and pos == 1):
-        return stock + ": " + get_news(stock)
-    elif(data >= 5 and pos == -1):
-        return stock + ": " + get_news(stock)
-    else:
-        return ""     
+	URL = "https://api.iextrading.com/1.0/stock/" + stock + "/quote/changePercent"
+	r = requests.get(url = URL)
+	
+	data = r.json()
+	data = data*100
+	if(data <= -5 and pos == 1):
+		return stock + ": " + get_news(stock)
+	elif(data >= 5 and pos == -1):
+		return stock + ": " + get_news(stock)
+	else:
+		return ""
 
 def main():
-    email_msg = ""
-    file = open("info.txt", "w")
-    stock = {"aapl", "goog", "msft", "mu", "tsla", "mtn", "ba", 
-             "baba", "cat", "unh", "mcd", "jpm", "hd", "axp", "wmt", 
-             "mmm", "gs", "v", "dwdp", "nke", "intc", "csco", "trv", 
-             "pfe", "jnj", "utx", "cvx", "spy", "qqq", "dis", "vz", 
-             "ko", "pg", "ibm", "xom", "mrk", "nflx", "stx", "isrg",}
+	email_msg = ""
+	file = open("info.txt", "w")
+	stock = {"aapl", "goog", "msft", "mu", "tsla", "mtn", "ba", 
+			 "baba", "cat", "unh", "mcd", "jpm", "hd", "axp", "wmt", 
+			 "mmm", "gs", "v", "dwdp", "nke", "intc", "csco", "trv", 
+			 "pfe", "jnj", "utx", "cvx", "spy", "qqq", "dis", "vz", 
+			 "ko", "pg", "ibm", "xom", "mrk", "nflx", "stx", "isrg",}
 
-    for i in stock:
-        URL = "https://api.iextrading.com/1.0/stock/" + i + "/stats/year1ChangePercent"
-        print("Retrieving information: " + i + "...")
-        r = requests.get(url = URL)
-    
-        data = r.json()
-        percent = data*100
-    
-        if percent >= 25:
-            file.write("Positive: " + i + "\n")
-            email_msg = email_msg + check_move(i, 1) + "\n"
-        elif percent <= -25:
-            file.write("Negative: " + i + "\n")
-            email_msg = email_msg + check_move(i, -1) + "\n"
-        
-    file.close()
-    if(email_msg != ""):
-        email_data(email_msg)
-    sys.exit("Finished Compiling Information. Exiting.\n")
-        
+	for i in stock:
+		URL = "https://api.iextrading.com/1.0/stock/" + i + "/stats/year1ChangePercent"
+		print("Retrieving information: " + i + "...")
+		r = requests.get(url = URL)
+	
+		data = r.json()
+		percent = data*100
+
+		if percent >= 25:
+			file.write("Positive: " + i + "\n")
+			info = check_move(i, 1)
+			if(info != ""):
+				email_msg = email_msg + info + "\n"
+		elif percent <= -25:
+			file.write("Negative: " + i + "\n")
+			info = check_move(i, -1)
+			if(info != ""):
+				email_msg = email_msg + info + "\n"
+		
+	file.close()
+	if(email_msg != ""):
+		email_data(email_msg)
+	sys.exit("Finished Compiling Information. Exiting.\n")
+		
 if __name__=='__main__':
-    main()
+	main()
